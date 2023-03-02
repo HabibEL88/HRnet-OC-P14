c0f9f177b1f06d0e7e06ab56bba7ccd1eae8e71f
HabibEL88 <elmouini.habib@gmail.com>
2023-03-02 02:01:01 +0100

    form creation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,46 +1,91 @@
 import React from "react";
+import "../style/form.css";
 
 const Form = () => {
   return (
-    <div>
-      <div>
-        <label for="first-name">First Name</label>
+    <div className="container">
+      <h2>Create Employee</h2>
+      <div className="field">
+        <input type="text" name="first-name" autoComplete="off" required />
+        <label htmlFor="first-name" className="label-wrapper">
+          <span className="label-text">First Name</span>
+        </label>
+      </div>
+      <div className="field">
+        <input type="text" name="last-name" autoComplete="off" required />
+        <label htmlFor="last-name" className="label-wrapper">
+          <span className="label-text">Last Name</span>
+        </label>
+      </div>
+      <div className="field">
+        <input type="text" name="date-of-birth" autoComplete="off" required />
+        <label htmlFor="date-of-birth" className="label-wrapper">
+          <span className="label-text">Date of Birth</span>
+        </label>
+      </div>
+      <div className="field">
+        <input type="text" name="start-date" autoComplete="off" required />
+        <label htmlFor="start-date" className="label-wrapper">
+          <span className="label-text">Start Date</span>
+        </label>
+      </div>
+      {/* <div>
+        <label htmlFor="first-name">First Name</label>
         <input type="text" id="first-name" />
       </div>
       <div>
-        <label for="last-name">Last Name</label>
+        <label htmlFor="last-name">Last Name</label>
         <input type="text" id="last-name" />
       </div>
 
       <div>
-        <label for="date-of-birth">Date of Birth</label>
+        <label htmlFor="date-of-birth">Date of Birth</label>
         <input id="date-of-birth" type="text" />
       </div>
-      <fieldset class="address">
+      <div>
+        <label htmlFor="start-date">Start Date</label>
+        <input id="start-date" type="text" />
+      </div> */}
+
+      <fieldset className="address">
         <legend>Address</legend>
 
-        <label for="street">Street</label>
-        <input id="street" type="text" />
+        <div className="field">
+          <input type="text" id="street" autoComplete="off" required />
+          <label htmlFor="street" className="label-wrapper">
+            <span className="label-text">Street</span>
+          </label>
+        </div>
 
-        <label for="city">City</label>
-        <input id="city" type="text" />
+        <div className="field">
+          <input type="text" id="city" autoComplete="off" required />
+          <label htmlFor="city" className="label-wrapper">
+            <span className="label-text">City</span>
+          </label>
+        </div>
 
-        <label for="state">State</label>
-        <select name="state" id="state"></select>
+        <div className="fieldBottom">
+          <label htmlFor="state">State</label>
+          <select name="state" id="state"></select>
+        </div>
 
-        <label for="zip-code">Zip Code</label>
-        <input id="zip-code" type="number" />
+        <div className="fieldBottom">
+          <label htmlFor="zip-code">Zip Code</label>
+          <input id="zip-code" type="number" />
+        </div>
+
+        <div className="fieldBottom">
+          <label htmlFor="department">Department</label>
+          <select name="department" id="department">
+            <option>Sales</option>
+            <option>Marketing</option>
+            <option>Engineering</option>
+            <option>Human Resources</option>
+            <option>Legal</option>
+          </select>
+        </div>
       </fieldset>
-      <div>
-        <label for="department">Department</label>
-        <select name="department" id="department">
-          <option>Sales</option>
-          <option>Marketing</option>
-          <option>Engineering</option>
-          <option>Human Resources</option>
-          <option>Legal</option>
-        </select>
-      </div>
+      <button>Save</button>
     </div>
   );
 };
