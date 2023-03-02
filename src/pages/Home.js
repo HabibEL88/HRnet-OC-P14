5a18e953c6b5b1ec52e25203a277f06877a209ca
HabibEL88 <elmouini.habib@gmail.com>
2023-03-02 02:01:55 +0100

    Page home done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Form from "../components/Form";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <main>
-      <h2>Create Employee</h2>
+      <Header />
       <Form />
+      <Footer />
     </main>
   );
 };
