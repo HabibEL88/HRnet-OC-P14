c90e5075e707dd722b25c27a4389b2187899f86a
HabibEL88 <elmouini.habib@gmail.com>
2023-03-05 23:42:00 +0100

    add mocked data

--- src/services/employees.js
+++ src/services/employees.js
@@ -1,6 +1,44 @@
 import React from "react";
 
-export const BASE_DATA = [{ name: "Habib" }, { name: "Aurélien" }];
+// Path: src/services/employees.js
+export const BASE_DATA = [
+  {
+    id: 1,
+    name: "Tiger",
+    LastName: "Nixon",
+    startDate: "18/03/2000",
+    Department: "Marketing",
+    birthDate: "02/02/1968",
+    Street: "allee adolphe pegoud",
+    City: "New-York",
+    State: "AL",
+    Zip: "10001",
+  },
+  {
+    id: 2,
+    name: "Garrett",
+    LastName: "Winters",
+    startDate: "18/03/2002",
+    Department: "Marketing",
+    birthDate: "02/02/1955",
+    Street: "allee streetworkout",
+    City: "New-York",
+    State: "AL",
+    Zip: "10002",
+  },
+  {
+    id: 3,
+    name: "Ashton",
+    LastName: "Cox",
+    startDate: "18/03/1998",
+    Department: "Marketing",
+    birthDate: "02/02/1970",
+    Street: "allee adolphe pegoud",
+    City: "washington",
+    State: "AL",
+    Zip: "10003",
+  },
+];
 
 export const EmployeeContext = React.createContext({
   employees: BASE_DATA,
